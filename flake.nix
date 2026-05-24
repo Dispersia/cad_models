0f9c95197722596133677f2c5f0b5459e1bc66d0
Jenny Housh <jennifer.ylee01@gmail.com>
2026-05-24 11:06:47 -0700

    Jenny's first contribution

--- flake.nix
+++ flake.nix
@@ -3,39 +3,42 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          uv
-          python312
-          pyright
-        ];
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        isLinux = pkgs.stdenv.isLinux;
+      in
+      {
+        devShells.default = pkgs.mkShell ({
+          packages = with pkgs; [
+            uv
+            python312
+            pyright
+          ];
 
-        shellHook = ''
-          if [ ! -d .venv ]; then
-            uv venv .venv --python python3.12
-            uv pip install build123d
-          fi
-          source .venv/bin/activate
-        '';
-
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
-          libGL
-          libGLU
-          libx11
-          libxrender
-          libxmu
-          stdenv.cc.cc.lib
-          expat
-          zlib
-        ]);
-      };
-    };
+          shellHook = ''
+            if [ ! -d .venv ]; then
+              uv venv .venv --python python3.12
+              uv pip install build123d
+            fi
+            source .venv/bin/activate
+          '';
+        } // pkgs.lib.optionalAttrs isLinux {
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
+            libGL
+            libGLU
+            libx11
+            libxrender
+            libxmu
+            stdenv.cc.cc.lib
+            expat
+            zlib
+          ]);
+        });
+      }
+    );
 }
