d84dbeb86ad9dbc3e963844319310bca3af3edf6
Jenny Cruz <jennifer.ylee01@gmail.com>
2026-06-14 22:07:04 -0700

    update

--- sewing_storage/threads.py
+++ sewing_storage/threads.py
@@ -1,9 +1,11 @@
 from build123d import *
 
-with BuildSketch() as box:
-
+with BuildPart() as box:
+    with BuildSketch() as box_sk:
+        Rectangle(4, 10)
+    extrude(amount=2)
 
 def main():
     return {
-            "box": box
-            }
+        "box": box
+    }
